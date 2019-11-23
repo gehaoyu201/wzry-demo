b1e9aca0d3fb629bc3549ea5f4243151be302ad3
gehaoyu <971019537@qq.com>
2019-11-23 17:53:52 +0800

    eight

--- admin/src/http.js
+++ admin/src/http.js
@@ -5,7 +5,7 @@ import router from './router'
 
 const http = axios.create({
     // baseURL: 'http://localhost:3000/admin/api/',
-    baseURL: process.env.VUE_APP_API_URL || '/admin/api'
+    baseURL: process.env.VUE_APP_API_URL || '/admin/api/'
 })
 
 http.interceptors.request.use(config => {
